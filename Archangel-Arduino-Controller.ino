4502308334155a59fbee0e43bfff2ca59e1f35e8
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2024-01-19 23:44:12 +0000

    Support for multiple servos at once

--- Archangel-Arduino-Controller.ino
+++ Archangel-Arduino-Controller.ino
@@ -1,15 +1,28 @@
+#include <Servo.h>
+
+const unsigned int N_SERVOS = 3;
+const unsigned int SERVO_PINS[N_SERVOS] = {9, 10, 11};
+Servo SERVOS[N_SERVOS];
+
+
 void setup(){
    
   // Set the baud rate  
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
+
+  //attach the servos
+  for (int i = 0; i < N_SERVOS; i++){
+    SERVOS[i].attach(SERVO_PINS[i]);
+    SERVOS[i].write(0);
+  }
    
 }
 
-unsigned int blink_delay = 100;
-
 void loop(){
  
+  int servo = -1;
+  int value = -1;
   if(Serial.available() > 0) {
     //faster reimplementation of Serial.readStringUntil
     char buffer[Serial.available() + 1];
@@ -22,15 +35,20 @@ void loop(){
       buffer[index] = Serial.read();
       index += 1;
     }
+    //ensure the string is properly terminated
     buffer[index] = "\0";
 
-    //don't forget to set the delay;
-    blink_delay = atoi(buffer);
+    //subtracting 0 gives us the difference in value between 0 and the given byte, 
+    //i.e. the numerical difference between the numbers the characters represent
+    servo = buffer[0] - '0';
+    //this skips a character (the one which we just used to get the servo index)
+    value = atoi(buffer + 1);
   }
 
-  delay(blink_delay);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(blink_delay);
-  digitalWrite(LED_BUILTIN, LOW);
+  if (servo != -1 && servo < N_SERVOS) {
+    SERVOS[servo].write(value);
+  }
 
+  //this doesn't need to run too often
+  delay(20);
 }
